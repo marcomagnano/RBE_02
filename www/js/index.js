ded4a2f4f57da2a766331da9ed2e9b4a9c7e2e43
Marco Magnano <roots.marco@gmail.com>
2013-09-03 13:00:15 +0200

    Full switch

--- www/js/index.js
+++ www/js/index.js
@@ -138,5 +138,217 @@ var app = {
 				localStorage.setItem('postData', JSON.stringify(data.posts[$(this).index()]));
 			});
 		});
+	},
+	VPblog: function() {
+		function getVPBlogs() {
+			$('#page-loader').fadeIn(600);
+			var dfd = $.Deferred();
+			$.ajax({
+				url: 'http://vociprotestanti.it/api/get_category_posts/?slug=news/',
+				type: 'GET',
+				dataType: 'json',
+				success: function(data) {
+					var allposts = data.posts,
+						buffy = '';
+					$.each(allposts, function(postid, postcontent) {
+						var basedate = new Date(postcontent.date),
+							basemonth = basedate.getMonth() + 1,
+							post_thumbnail = '';
+						var itadate = basedate.getDate() + ' ';
+						switch (basemonth) {
+						case 1:
+							itadate += 'gennaio';
+							break;
+						case 2:
+							itadate += 'febbraio';
+							break;
+						case 3:
+							itadate += 'marzo';
+							break;
+						case 4:
+							itadate += 'aprile';
+							break;
+						case 5:
+							itadate += 'maggio';
+							break;
+						case 6:
+							itadate += 'giugno';
+							break;
+						case 7:
+							itadate += 'luglio';
+							break;
+						case 8:
+							itadate += 'agosto';
+							break;
+						case 9:
+							itadate += 'settembre';
+							break;
+						case 10:
+							itadate += 'ottobre';
+							break;
+						case 11:
+							itadate += 'novembre';
+							break;
+						case 12:
+							itadate += 'dicembre';
+							break;
+						}
+						itadate += ' ' + basedate.getFullYear();
+						$.each(postcontent.attachments, function(attid, attcontent) {
+							if (attcontent.mime_type == 'image/jpeg') {
+								post_image = attcontent.url;
+								return false;
+							}
+						});
+						if (post_image == '') {
+							post_thumbnail = 'img/noimage.png';
+						} else {
+							post_thumbnail = post_image.replace('.jpg', '-150x150.jpg');
+						}
+						buffy += '<li><a data-transition="slide" href="vp-detail.html?' + postid + '">';
+						buffy += '<img class="post_thumbnail" src="' + post_thumbnail + '" width="100" height="100" title="' + postcontent.title + '" />';
+						buffy += '<h2 class="entry-title">' + postcontent.title + '</h2>';
+						buffy += '<span class="listingmeta">' + itadate + ' - ' + postcontent.comment_count + ' commenti</span>';
+						buffy += '</a></li>';
+					});
+					$('#all-posts').html(buffy);
+					//var source   = $("#blog-template").html();
+					//var template = Handlebars.compile(source);
+					//var blogData = template(data);
+					//$('#blog-data').html(blogData);
+					//$('#blog-data').trigger('create');
+					dfd.resolve(data);
+				},
+				error: function(data) {
+					console.log(data);
+				}
+			});
+			return dfd.promise();
+		};
+		getVPBlogs().then(function(data) {
+			$('#page-loader').fadeOut(600);
+			$('#all-posts').on('click', 'li', function(e) {
+				localStorage.setItem('postData', JSON.stringify(data.posts[$(this).index()]));
+			});
+		});
+	},
+	single: function() {
+		var postDataStorage = localStorage.getItem('postData'),
+			postData = JSON.parse(postDataStorage),
+			buffy = '',
+			content = postData.content.replace('<a ', '<a target="_blank" ');
+		var post_thumbnail = postData.thumbnail;
+		if (post_image != null) {
+			var post_image = post_thumbnail.replace('-150x150.jpg', '.jpg');
+		}
+		var basedate = new Date(postData.date),
+			basemonth = basedate.getMonth() + 1,
+			post_thumbnail = '',
+			itadate = basedate.getDate() + ' ';
+		switch (basemonth) {
+		case 1:
+			itadate += 'gennaio';
+			break;
+		case 2:
+			itadate += 'febbraio';
+			break;
+		case 3:
+			itadate += 'marzo';
+			break;
+		case 4:
+			itadate += 'aprile';
+			break;
+		case 5:
+			itadate += 'maggio';
+			break;
+		case 6:
+			itadate += 'giugno';
+			break;
+		case 7:
+			itadate += 'luglio';
+			break;
+		case 8:
+			itadate += 'agosto';
+			break;
+		case 9:
+			itadate += 'settembre';
+			break;
+		case 10:
+			itadate += 'ottobre';
+			break;
+		case 11:
+			itadate += 'novembre';
+			break;
+		case 12:
+			itadate += 'dicembre';
+			break;
+		}
+
+		itadate += ' ' + basedate.getFullYear();
+		buffy += '<h1 class="entry-title">' + postData.title + '</h1>';
+		buffy += '<span class="entry-date">' + itadate + '</span>';
+		if (post_thumbnail != '' && post_thumbnail != null) {
+			buffy += '<img src="' + post_image + '" class="post_thumbnail" />';
+		}
+		buffy += '<div class="entry-content">' + content + '</div>';
+		$('#single-data').html(buffy);
+	},
+	VPsingle: function() {
+		var postDataStorage = localStorage.getItem('postData'),
+			postData = JSON.parse(postDataStorage),
+			buffy = '',
+			content = postData.content.replace('<a ', '<a target="_blank" ');
+		var post_thumbnail = postData.thumbnail,
+			post_image = post_thumbnail.replace('-150x150.jpg', '.jpg');
+		var basedate = new Date(postData.date),
+			basemonth = basedate.getMonth() + 1,
+			post_thumbnail = '',
+			itadate = basedate.getDate() + ' ';
+		switch (basemonth) {
+		case 1:
+			itadate += 'gennaio';
+			break;
+		case 2:
+			itadate += 'febbraio';
+			break;
+		case 3:
+			itadate += 'marzo';
+			break;
+		case 4:
+			itadate += 'aprile';
+			break;
+		case 5:
+			itadate += 'maggio';
+			break;
+		case 6:
+			itadate += 'giugno';
+			break;
+		case 7:
+			itadate += 'luglio';
+			break;
+		case 8:
+			itadate += 'agosto';
+			break;
+		case 9:
+			itadate += 'settembre';
+			break;
+		case 10:
+			itadate += 'ottobre';
+			break;
+		case 11:
+			itadate += 'novembre';
+			break;
+		case 12:
+			itadate += 'dicembre';
+			break;
+		}
+		itadate += ' ' + basedate.getFullYear();
+		buffy += '<h1 class="entry-title">' + postData.title + '</h1>';
+		buffy += '<span class="entry-date">' + itadate + '</span>';
+		if (post_thumbnail != '') {
+			buffy += '<img src="' + post_image + '" class="post_thumbnail" />';
+		}
+		buffy += '<div class="entry-content">' + content + '</div>';
+		$('#single-data').html(buffy);
 	}
 };
